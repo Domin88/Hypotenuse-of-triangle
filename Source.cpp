ce41c415c58c05e116e5834da3956d0dc094483d
David Injikulyan <97161776+Domin88@users.noreply.github.com>
2022-12-12 15:44:56 +0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+
+double f(double x, double y) {
+
+	
+
+	if (x == 0 || y == 0)
+		std::cout << "Error:: you write wrong number" << std::endl;
+	else
+		return pow(x,2) + pow(y,2);
+
+}
+
+int main() {
+
+	int x, y;
+
+	std::cout << "Please enter 1st side of triangle : \n" << std::endl;
+	std::cin >> x;
+
+	std::cout << "Please enter 2nd side of triangle : \n" << std::endl;
+	std::cin >> y;
+
+	std::cout <<"Hypotenuse of triangle equal to "<< sqrt(f(x, y)) << std::endl;
+
+	return 0;
+	
+
+
+
+}
